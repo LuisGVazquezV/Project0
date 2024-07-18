a70061ce7faa87683cf3c468e98667cd80903226
LuisGVazquezV <luis.vazquez51097@gmail.com>
2024-07-18 01:43:27 -0400

    worked on User DAO and DAO interface.

--- src/main/java/com/revature/DAOs/UserDAO.java
+++ src/main/java/com/revature/DAOs/UserDAO.java
@@ -1,4 +1,46 @@
 package com.revature.DAOs;
 
-public class UserDAO {
+import com.revature.models.User;
+import com.revature.utils.ConnectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class UserDAO implements UserDAOInterface {
+
+    @Override
+    public ArrayList<User> getUsers() {
+
+        try (Connection conn = ConnectionUtil.getConnection()) {
+
+            String sql = "SELECT * FROM users";
+            Statement s = conn.createStatement();
+            ResultSet rs = s.executeQuery(sql);
+
+            ArrayList<User> users = new ArrayList<>();
+
+            while (rs.next()) {
+                User u = new User(
+                        rs.getInt("user_id"),
+                        rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getString("email"),
+                        rs.getString("phone_number"),
+                        rs.getString("address")
+                );
+
+                users.add(u);
+            }
+            return users;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public User insertUser(User user) {
+        return null;
+    }
 }
--- src/main/java/com/revature/DAOs/UserDAOInterface.java
+++ src/main/java/com/revature/DAOs/UserDAOInterface.java
@@ -1,4 +1,13 @@
 package com.revature.DAOs;
 
+import com.revature.models.User;
+
+import java.util.ArrayList;
+
 public interface UserDAOInterface {
+
+    public ArrayList<User> getUsers();
+
+    User insertUser(User user);
+
 }
